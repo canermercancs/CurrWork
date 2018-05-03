38db1b38f9afe4ef6fa06272819f58ab9f9252d7
canermercancs <caner.mercan@cs.bilkent.edu.tr>
2018-05-03 20:09:03 +0300

    added new visualizations for params across epochs

--- plotProgress.m
+++ plotProgress.m
@@ -1,5 +1,35 @@
 clear
-load hcrfPIM_(aux_whl(byH)_v4)_testcv(1)_validcv(2)_proxy(1).mat
+load hcrfPIM_(aux_lclNOtrain_v0)_testcv(1)_validcv(2)_proxy(1).mat
+
+%%
+
+allparams = [params_all{1:curr_epoch}];
+allparams = reshape(allparams, 3, length(allparams)/3);
+
+figure, 
+for i = 1:curr_epoch
+    imagesc(params_all{i}{1});
+    title(sprintf('epoch #%i', i));
+    colorbar
+    hold on
+    pause(0.2)
+end
+
+%%
+fig_ttl = {'-vs-','-vs+', '+vs-', '+vs+'};
+figure,
+for i=1:curr_epoch
+    for j=1:4
+        subplot(1,4,j);
+        imagesc(params_all{i}{3}(:,:,j));
+        title(fig_ttl{j});
+    end
+%     title(sprintf('epoch #%i', i));
+    hold on;
+    pause(0.2);
+end
+
+
 %%
 if ~exist('curr_epoch', 'var')
     curr_epoch = find(cellfun(@isempty, params_all), 1) - 1;
